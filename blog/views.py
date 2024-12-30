b189c122b4fe65999ea9cff82f3b2033f6d1768d
Gree606 <sleya7110@gmail.com>
2024-12-30 12:45:05 +0530

    Added templated for home and about html. Also added base.html to include reusable tags

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=[
+  {
+    'title':'First Post',
+    'author': 'Gree',
+    'date':'30th Dec 2024',
+    'content':' This is a dummy data not taken from DB',
+
+  },
+  {
+    'title':'Second Post',
+    'author':'Greeshma',
+    'date': '31st Dec 2024',
+    'content':'This post was created on the last day of the year',
+  }
+]
+
+#Handles navigation routes
 def home(request):
-  return HttpResponse('<h1> Blog Homepage </h1>')
+  #To pass the contenst from this code as arguments into the html page
+  context={
+    'posts':posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  return HttpResponse('<h1> Blog About </h1>')
+  return render(request, 'blog/about.html', {'title':"About"})
 
