fb499c6e68cc188dd8c3fe0ab229dddf422a2817
Gree606 <sleya7110@gmail.com>
2024-12-31 16:26:31 +0530

    data added from database

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts=[
-  {
-    'title':'First Post',
-    'author': 'Gree',
-    'date':'30th Dec 2024',
-    'content':' This is a dummy data not taken from DB',
+# posts=[
+#   {
+#     'title':'First Post',
+#     'author': 'Gree',
+#     'date':'30th Dec 2024',
+#     'content':' This is a dummy data not taken from DB',
 
-  },
-  {
-    'title':'Second Post',
-    'author':'Greeshma',
-    'date': '31st Dec 2024',
-    'content':'This post was created on the last day of the year',
-  }
-]
+#   },
+#   {
+#     'title':'Second Post',
+#     'author':'Greeshma',
+#     'date': '31st Dec 2024',
+#     'content':'This post was created on the last day of the year',
+#   }
+# ]
 
 #Handles navigation routes
 def home(request):
   #To pass the contenst from this code as arguments into the html page
   context={
-    'posts':posts
+    'posts':Post.objects.all()
   }
   return render(request, 'blog/home.html', context)
 
